3b45caac834f57f027c18792bd6b09ea3a2bbb78
Nataxaxa <rconatalia@gmail.com>
2023-04-14 19:37:55 +0300

    lab3

---
+++ MaksimkinaNA/screens/SelectEventScreen/SelectEventScreen.jsx
@@ -0,0 +1,38 @@
+import { View } from "react-native";
+import {gql, useLazyQuery } from "@apollo/client"
+import { DatePicker } from "../../components/DatePicker";
+
+const GET_EVENT_DATE = gql`
+    query getEventDate($year: Int, $month: Int, $day: int) {
+        eventDate(year: $year, month: $month, day: $day) {
+            name
+            description
+        }
+    }
+`
+
+export const SelectEventScreen = () => {
+    const [loadEvent, { loading, data}] = useLazyQuery(GET_EVENT_DATE);
+    
+    const handleConfirmDatePicker = (value) => {
+
+        console.log(value);
+
+        loadEvent({
+            variables: {
+                year: Number(value[0]),
+                month: Number(value[1]),
+                day: Number(value[3]),
+            },
+        });
+        
+    }
+    
+    
+    
+    return (
+        <View>
+            <DatePicker onConfirm={handleConfirmDatePicker} />
+        </View>
+    )
+}
